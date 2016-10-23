9065265d641f29e447716cc27447d8f3526170c9
Tirthal Patel <patel.tirthal@gmail.com>
2016-10-23 15:18:52 +0530

    liferay-7-samples: Did minor cosmetic changes

--- liferay-7-samples/gs-dxp-workspace/modules/hello-world-mvc-portlet/src/main/java/com/tirthal/learning/HelloWorldMvcPortlet.java
+++ liferay-7-samples/gs-dxp-workspace/modules/hello-world-mvc-portlet/src/main/java/com/tirthal/learning/HelloWorldMvcPortlet.java
@@ -1,10 +1,26 @@
 package com.tirthal.learning;
 
-import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import java.io.IOException;
 
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+/*
+ * Notes:
+ * (1) @Component --- defines the class as a Service Component: a provider of a particular service class
+ * (2) immediate=true --- when this module is deployed and all of its dependencies are satisfied, it should be started immediately instead of being lazy-loaded
+ * (3) property { *.portlet.* } --- portlet specific properties such as the category in which it should appear in Liferay’s UI, its display name, its default view, and more. 
+ * (4) service=Portlet.class --- the service (which is just a Java Interface) that it implements is defined, which is the portlet class.  
+ */
 
 @Component(
 	immediate = true,
@@ -20,4 +36,16 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class HelloWorldMvcPortlet extends MVCPortlet {
+	
+	@Override
+	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+		User user = themeDisplay.getUser();
+
+		renderRequest.setAttribute("userName", user.getFirstName());
+
+		super.render(renderRequest, renderResponse);
+	}
+
 }
--- liferay-7-samples/gs-dxp-workspace/modules/hello-world-mvc-portlet/src/main/resources/META-INF/resources/view.jsp
+++ liferay-7-samples/gs-dxp-workspace/modules/hello-world-mvc-portlet/src/main/resources/META-INF/resources/view.jsp
@@ -1,5 +1,12 @@
-<%@ include file="/init.jsp" %>
+<%@ include file="/init.jsp"%>
+
+<jsp:useBean id="userName" type="java.lang.String" scope="request" />
+<p>
+	<b>Hello <%=userName%>!</b>
+</p>
 
 <p>
-	<b><liferay-ui:message key="hello_world_mvc_portlet_HelloWorldMvcPortlet.caption"/></b>
+	<b><liferay-ui:message key="hello_world_mvc_portlet_HelloWorldMvcPortlet.caption" /></b>
 </p>
+
+
